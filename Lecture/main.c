a4a2611c5e37c124fe5b999d884e0ff5ef5a6fc9
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2024-09-26 15:36:28 +0400

    Add a loop

--- Lecture/main.c
+++ Lecture/main.c
@@ -14,37 +14,40 @@ int main()
     int firstOperand;
 
     int secondOperand;
-  
+
     int operation;
 
-    puts("-----Welcome-----\nEnter first number: ");
-
-    scanf("%d", &firstOperand);
-
-    puts("\nEnter second number: ");
-    
-    scanf("%d", &secondOperand);
-    
-    puts("\nSelect operation: \n\n1 to Add\n2 to Substract\n3 to multiply\n4 to divide");
-
-    scanf("%d", &operation);
-
-    if (operation == Add)
+    do
     {
-        printf("\nResult: %d", AddNumbers(firstOperand, secondOperand));
-    }
-    else if (operation == Substract)
-    {
-        printf("\nResult: %d", SubstractNumbers(firstOperand, secondOperand));
-    }
-    else if (operation == Multiply)
-    {
-        printf("\nResult: %d", MultiplyNumbers(firstOperand, secondOperand));
-    }
-    else if (operation == Divide)
-    {
-        printf("\nResult: %d", DivideNumbers(firstOperand, secondOperand));
-    }
+        puts("\n\n-----Welcome-----\nEnter first number: ");
+
+        scanf("%d", &firstOperand);
+
+        puts("\nEnter second number: ");
+
+        scanf("%d", &secondOperand);
+
+        puts("\nSelect operation: \n\n1 to Add\n2 to Substract\n3 to multiply\n4 to divide\n-1 to exit");
+
+        scanf("%d", &operation);
+
+        if (operation == Add)
+        {
+            printf("\nResult: %d", AddNumbers(firstOperand, secondOperand));
+        }
+        else if (operation == Substract)
+        {
+            printf("\nResult: %d", SubstractNumbers(firstOperand, secondOperand));
+        }
+        else if (operation == Multiply)
+        {
+            printf("\nResult: %d", MultiplyNumbers(firstOperand, secondOperand));
+        }
+        else if (operation == Divide)
+        {
+            printf("\nResult: %d", DivideNumbers(firstOperand, secondOperand));
+        }
+    } while (operation != -1);
 
     return 0;
 }
